730c51b31a14340e6c52586d0f59d2780fadbf9e
heyingcai <heyingcai@fcbox.com>
2021-05-14 10:14:47 +0800

    add Leetcode

---
+++ src/main/java/com/jibug/learning/algorithm/leetcode/linkedlist/GetKthFromEnd.java
@@ -0,0 +1,44 @@
+package com.jibug.learning.algorithm.leetcode.linkedlist;
+
+import com.jibug.learning.datastructure.linked.ListNode;
+
+/**
+ * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+ * <p>
+ * 例如，一个链表有 6 个节点，从头节点开始，它们的值依次是 1、2、3、4、5、6。这个链表的倒数第 3 个节点是值为 4 的节点。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 给定一个链表: 1->2->3->4->5, 和 k = 2.
+ * <p>
+ * 返回链表 4->5.
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class GetKthFromEnd {
+
+    /**
+     * 快慢指针，快指针先走K步，然后再一起走到最后，慢指针就是倒数第K个节点
+     *
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode fast = head, slow = head;
+        while (fast != null && k != 0) {
+            fast = fast.next;
+            k--;
+        }
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+}
