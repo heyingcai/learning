258a894647d628dbb2ddfc3198d7eb28ffca4458
何英才（003613） <heyingcai@fcbox.com>
2021-06-03 17:36:18 +0800

    add leetcode

---
+++ src/main/java/com/jibug/learning/algorithm/leetcode/tree/LowestCommonAncestor_236.java
@@ -0,0 +1,35 @@
+package com.jibug.learning.algorithm.leetcode.tree;
+
+import com.jibug.learning.datastructure.tree.TreeNode;
+
+/**
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class LowestCommonAncestor_236 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == q || root == p) return root;
+
+        // 后序遍历
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        //左右子树都有则返回root
+        if (left != null && right != null) {
+            return root;
+        } else {
+            //如果左右子树中有一个子问题没得到结果，则返回得到结果的子问题.
+            return left != null ? left : right;
+        }
+    }
+
+}
