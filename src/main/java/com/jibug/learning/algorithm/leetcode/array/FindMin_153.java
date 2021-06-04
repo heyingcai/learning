19ca360c9a8b78d5123ba1dc1d1c62bc13ace03e
何英才（003613） <heyingcai@fcbox.com>
2021-06-04 18:55:35 +0800

    add leetcode

---
+++ src/main/java/com/jibug/learning/algorithm/leetcode/array/FindMin_153.java
@@ -0,0 +1,46 @@
+package com.jibug.learning.algorithm.leetcode.array;
+
+/**
+ * 已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。例如，原数组 nums = [0,1,2,4,5,6,7] 在变化后可能得到：
+ * 若旋转 4 次，则可以得到 [4,5,6,7,0,1,2]
+ * 若旋转 7 次，则可以得到 [0,1,2,4,5,6,7]
+ * 注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+ * <p>
+ * 给你一个元素值 互不相同 的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [3,4,5,1,2]
+ * 输出：1
+ * 解释：原数组为 [1,2,3,4,5] ，旋转 3 次得到输入数组。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FindMin_153 {
+
+    public int findMin(int[] nums) {
+        return find(nums, 0, nums.length - 1);
+    }
+
+    private int find(int[] nums, int left, int right) {
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            // 如果右边的值大于基准值，说明分界点在mid左边
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            }
+            // 如果右边的值小于基准值，说明分界点在mid右边
+            else if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right--;
+            }
+        }
+        return nums[left];
+    }
+
+}
